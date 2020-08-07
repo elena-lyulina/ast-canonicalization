3053d77219a025641921b7e5f93327cbed6ab74b
nbirillo <nbirillo@mail.ru>
2020-08-07 16:50:10 +0300

    Rename tree -> treeCtx

--- src/main/kotlin/org/jetbrains/research/transformations/Transformation.kt
+++ src/main/kotlin/org/jetbrains/research/transformations/Transformation.kt
@@ -11,16 +11,16 @@ interface Transformation {
 
     /**
      * Applying forward transformation.
-     * @param [tree] - Abstract syntax tree, built on a source code.
+     * @param [treeCtx] - Abstract syntax tree, built on a source code.
      * @param [toStoreMetadata] - flag that indicates necessity
      * of metadata storing.
      */
-    fun apply(tree: TreeContext, toStoreMetadata: Boolean)
+    fun apply(treeCtx: TreeContext, toStoreMetadata: Boolean)
 
     /**
      * Applying reverse transformation.
-     * @param [tree] - Abstract syntax tree, built on source code.
+     * @param [treeCtx] - Abstract syntax tree, built on source code.
      */
-    fun reverseApply(tree: TreeContext)
+    fun reverseApply(treeCtx: TreeContext)
 
 }
