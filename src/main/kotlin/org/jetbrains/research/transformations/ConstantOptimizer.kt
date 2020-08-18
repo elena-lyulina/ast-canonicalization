f0bf51f2e294f9b4eb3b434bdbd430892694ded0
aniskoff <cme412@mail.ru>
2020-08-18 22:31:25 +0300

    change architecture

--- src/main/kotlin/org/jetbrains/research/transformations/ConstantOptimizer.kt
+++ src/main/kotlin/org/jetbrains/research/transformations/ConstantOptimizer.kt
@@ -1,19 +1,33 @@
 package org.jetbrains.research.transformations
 
+import com.github.gumtreediff.tree.ITree
 import com.github.gumtreediff.tree.TreeContext
 
-object ConstantOptimizer: Transformation {
+object ConstantOptimizer: Transformation, NodeTransformer() {
     override val metadataKey: String
         get() = TODO("Not yet implemented")
 
+
     override fun apply(treeCtx: TreeContext, toStoreMetadata: Boolean) {
-        val tree = treeCtx.root
-        val treeIterator = tree.trees.iterator()
+        treeCtx.root = visit(treeCtx.root, treeCtx)
+    }
+
+    override fun visit(node: ITree, treeCtx: TreeContext): ITree {
+        super.genericVisit(node, treeCtx)
+
+        if (treeCtx.getTypeLabel(node) != "BinOp_Add")
+            return node
+
+        val left = node.getChild(0)
+        val right = node.getChild(1)
+        val typeLabel: String = treeCtx.getTypeLabel(left)
+        val newVal = left.label.toInt() + right.label.toInt()
+
+        return treeCtx.createTree(left.type, newVal.toString(), typeLabel)
 
     }
 
     override fun reverseApply(treeCtx: TreeContext) {
         TODO("Not yet implemented")
     }
-
 }
