76eabf53511ccf1918a6fce7f58820e7c0779835
aniskoff <cme412@mail.ru>
2020-08-18 22:48:02 +0300

    borrow ideas of interface of transformations from python ast library

---
+++ src/main/kotlin/org/jetbrains/research/transformations/NodeTransformer.kt
@@ -0,0 +1,18 @@
+package org.jetbrains.research.transformations
+
+import com.github.gumtreediff.tree.ITree
+import com.github.gumtreediff.tree.TreeContext
+
+open class NodeTransformer : NodeVisitor {
+    override fun visit(node: ITree, treeCtx: TreeContext) : ITree {
+        return node
+    }
+
+    override fun reverseVisit(node: ITree, treeCtx: TreeContext): ITree {
+        return node
+    }
+
+    override fun genericVisit(node: ITree, treeCtx: TreeContext) {
+        node.children  = node.children?.map { visit(it, treeCtx) }
+    }
+}
---
+++ src/main/kotlin/org/jetbrains/research/transformations/NodeVisitor.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.research.transformations
+import com.github.gumtreediff.tree.ITree
+import com.github.gumtreediff.tree.TreeContext
+
+interface NodeVisitor {
+
+    fun visit(node: ITree, treeCtx: TreeContext) : ITree
+
+    fun reverseVisit(node: ITree, treeCtx: TreeContext) : ITree
+
+    fun genericVisit(node: ITree, treeCtx: TreeContext)
+
+}
