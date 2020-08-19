2989e8f559a23766577d609b8e6d1d34fa2c5bf1
aniskoff <cme412@mail.ru>
2020-08-19 16:20:02 +0300

    deleted extra file

--- src/main/kotlin/org/jetbrains/research/transformations/NodeTransformer.kt
+++ src/main/kotlin/org/jetbrains/research/transformations/NodeTransformer.kt
@@ -3,20 +3,16 @@ package org.jetbrains.research.transformations
 import com.github.gumtreediff.tree.ITree
 import com.github.gumtreediff.tree.TreeContext
 
-open class NodeTransformer : NodeVisitor {
-    override fun visit(node: ITree, treeCtx: TreeContext) : ITree {
-        return node
-    }
+abstract class NodeTransformer  {
+    protected abstract fun visit(node: ITree, treeCtx: TreeContext) : ITree
 
-    override fun reverseVisit(node: ITree, treeCtx: TreeContext): ITree {
-        return node
-    }
+    protected abstract fun reverseVisit(node: ITree, treeCtx: TreeContext): ITree
 
-    override fun genericVisit(node: ITree, treeCtx: TreeContext) {
+    protected fun genericVisit(node: ITree, treeCtx: TreeContext) {
         node.children = node.children?.map { visit(it, treeCtx) }
     }
 
-    override fun genericReverseVisit(node: ITree, treeCtx: TreeContext) {
+    protected fun genericReverseVisit(node: ITree, treeCtx: TreeContext) {
         node.children = node.children?.map { reverseVisit(it, treeCtx) }
     }
 }
