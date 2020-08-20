2dadfa696f897588460f00590166782290e3c3ae
RasAV <brother.sd@mail.ru>
2020-08-20 17:32:15 +0300

    made improvements in Anonymization.kt in the code style and fixed small mistakes. Also updated gradle-wrapper.properties

--- src/main/kotlin/org/jetbrains/research/transformations/Anonymization.kt
+++ src/main/kotlin/org/jetbrains/research/transformations/Anonymization.kt
@@ -6,70 +6,78 @@ import com.github.gumtreediff.tree.ITree
 import com.github.gumtreediff.tree.Tree
 import com.github.gumtreediff.tree.TreeContext
 
+enum class NodeType(val type: String) {
+    NAME_STORE("NameStore"),
+    NAME_LOAD("NameLoad")
+}
+
 object Anonymization: Transformation {
     override val metadataKey = "anonymization"
-    private var anonymNamesMap: MutableMap<String, String> = mutableMapOf()
-    private var numOfVariables = 0
     private const val VAR_PREFIX = "v"
 
 
     override fun apply(treeCtx: TreeContext, toStoreMetadata: Boolean) {
-        anonymNamesMap = mutableMapOf()
-        numOfVariables = 0
+        val anonymNamesMap: MutableMap<String, String> = mutableMapOf()
+        var numOfVariables = 0
         val tree = treeCtx.root
         val treeIterator = tree.trees.iterator()
 
         while (treeIterator.hasNext()) {
             val node = treeIterator.next()
 
-            if (treeCtx.getTypeLabel(node) == "NameStore") {
-                anonymNamesMap.getOrPut(node.label, { setAnonLabel(node, toStoreMetadata) })
-            }
-
-            if (treeCtx.getTypeLabel(node) == "NameLoad") {
-                if (node.label in anonymNamesMap.keys) {
-                    val newLabel = anonymNamesMap[node.label]
-                    if (toStoreMetadata) node.setMetadata(metadataKey, mutableMapOf(newLabel to node.label))
-                    node.label = newLabel
+            val nodeType = treeCtx.getTypeLabel(node)
+            when (nodeType) {
+                NodeType.NAME_STORE.type -> {
+                    anonymNamesMap.getOrPut(node.label, {
+                        numOfVariables += 1
+                        return@getOrPut(setAnonLabel(node, toStoreMetadata, numOfVariables))
+                    })
                 }
 
+                NodeType.NAME_LOAD.type -> {
+                    if (node.label in anonymNamesMap.keys) {
+                        val newLabel = anonymNamesMap[node.label]
+                        if (toStoreMetadata) node.setMetadata(metadataKey, mutableMapOf(newLabel to node.label))
+                        node.label = newLabel
+                    }
+                }
+                else -> {
+                    // default actions, for example log a message
+                }
             }
-        }
 
-        return
+
+        }
     }
 
-    private fun setAnonLabel(node: ITree, toStoreMetadata: Boolean) : String {
+    private fun setAnonLabel(node: ITree, toStoreMetadata: Boolean, numOfVariables: Int) : String {
         val newLabel = "$VAR_PREFIX$numOfVariables"
         if (toStoreMetadata) node.setMetadata(metadataKey, mutableMapOf(newLabel to node.label))
         node.label = newLabel
-        numOfVariables += 1
         return newLabel
     }
 
-    override fun reverseApply(treeCtx: TreeContext) {
+    override fun inverseApply(treeCtx: TreeContext) {
         val tree = treeCtx.root
         val treeIterator = tree.trees.iterator()
 
         while (treeIterator.hasNext()) {
             val node = treeIterator.next()
             if (treeCtx.getTypeLabel(node) == "NameStore") {
-
-                node?.getMetadata(metadataKey)?.let {
-                    val nodeMap = it as? MutableMap<String, String> ?:
-                    throw IllegalArgumentException("Value with key $metadataKey should be a map")
-                    node.label = nodeMap[node.label]
-                }
+                restoreLabelName(node)
             }
 
-            if (treeCtx.getTypeLabel(node) == "NameLoad" && node.label in anonymNamesMap.values) {
-
-                node?.getMetadata(metadataKey)?.let {
-                    val nodeMap = it as? MutableMap<String, String> ?:
-                    throw IllegalArgumentException("Value with key $metadataKey should be a map")
-                    node.label = nodeMap[node.label]
-                }
+            if (treeCtx.getTypeLabel(node) == "NameLoad") {
+                restoreLabelName(node)
             }
+        }
+    }
+
+    private fun restoreLabelName(node: ITree) {
+        node.getMetadata(metadataKey)?.let {
+            val nodeMap = it as? MutableMap<String, String> ?:
+            throw IllegalArgumentException("Value with key $metadataKey should be a map")
+            node.label = nodeMap[node.label]
         }
     }
 }
