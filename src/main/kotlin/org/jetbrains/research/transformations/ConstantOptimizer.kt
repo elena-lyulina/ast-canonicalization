72f76075e6bb133f7c709b1f6b3c10e2c6f18400
aniskoff <cme412@mail.ru>
2020-08-27 23:58:22 +0300

    Working (not ideal) version of constant folding transformation

--- src/main/kotlin/org/jetbrains/research/transformations/ConstantOptimizer.kt
+++ src/main/kotlin/org/jetbrains/research/transformations/ConstantOptimizer.kt
@@ -3,10 +3,12 @@ package org.jetbrains.research.transformations
 import com.github.gumtreediff.tree.ITree
 import com.github.gumtreediff.tree.TreeContext
 
-object ConstantOptimizer: RecursiveTransformation() {
+class ConstantOptimizer(treeCtx: TreeContext): RecursiveTransformation() {
+
+    private val treeExplorer: TreeExplorer = TreeExplorer(treeCtx)
+
     override val metadataKey: String
         get() = TODO("Not yet implemented")
-
 
     override fun apply(treeCtx: TreeContext, toStoreMetadata: Boolean) {
         treeCtx.root = visit(treeCtx.root, treeCtx)
@@ -19,16 +21,35 @@ object ConstantOptimizer: RecursiveTransformation() {
     override fun visit(node: ITree, treeCtx: TreeContext): ITree {
         super.visitChildren(node, treeCtx)
 
-        if (treeCtx.getTypeLabel(node) != "BinOp_Add")
+        if (!treeExplorer.isBinOp(node)) {
             return node
+        }
 
+        val opName = treeExplorer.getOperationName(node)
         val left = node.getChild(0)
         val right = node.getChild(1)
-        val typeLabel: String = treeCtx.getTypeLabel(left)
-        val newVal = left.label.toInt() + right.label.toInt()
 
-        return treeCtx.createTree(left.type, newVal.toString(), typeLabel)
+        when {
+            treeExplorer.isConstLikeType(left) and treeExplorer.isConstLikeType(right) -> {
+                val opResult: OperationResult? = treeExplorer.getBinOpResult(opName, left, right)
+                val resTypeLabel = treeExplorer.getConstLikeTypeName(left) + "-" + opResult!!.resultPythonTypeName
+                val resTypeHash = resTypeLabel.hashCode()
 
+                return treeCtx.createTree(resTypeHash, opResult.result.toString(), resTypeLabel)
+            }
+
+//            treeExplorer.isConstLikeType(left) -> {
+//
+//                if (treeExplorer.getOperationName(node) in listOf("Mult", "Pow")) {
+//                    TODO()
+//                }
+//            }
+//
+//            treeExplorer.isConstLikeType(right) -> {
+//                TODO()
+//            }
+            else -> return node
+        }
     }
 
     override fun inverseVisit(node: ITree, treeCtx: TreeContext): ITree {
